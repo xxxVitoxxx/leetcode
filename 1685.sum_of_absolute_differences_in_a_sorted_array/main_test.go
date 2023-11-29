007c50b1cf0d13a6a29f6c1a9a00bd5d49131786
Vito <vito.chaooo@gmail.com>
2023-11-30 01:18:07 +0800

    chore: leetcode1685 (#70)

---
+++ 1685.sum_of_absolute_differences_in_a_sorted_array/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetSumAbsoluteDifferences(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  []int
+		output []int
+	}{
+		{"example1", []int{2, 3, 5}, []int{4, 3, 5}},
+		{"example2", []int{1, 4, 6, 8, 10}, []int{24, 15, 13, 15, 21}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := getSumAbsoluteDifferences(tt.input)
+			assert.Equal(t, tt.output, res)
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := getSumAbsoluteDifferences2(tt.input)
+			assert.Equal(t, tt.output, res)
+		})
+	}
+}
