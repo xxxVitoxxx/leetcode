74a1cd8c6acd56989dd1fb2e959cf138e4d1b89c
xxxVitoxxx <bird06291994@gmail.com>
2024-01-09 23:57:12 +0800

    test: add test case

--- 450.delete_node_in_a_bst/main_test.go
+++ 450.delete_node_in_a_bst/main_test.go
@@ -58,6 +58,84 @@ func TestDeleteNode(t *testing.T) {
 		},
 	}
 
+	tree3 := &TreeNode{
+		Val: 5,
+		Left: &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val: 1,
+			},
+		},
+	}
+	ans3 := &TreeNode{
+		Val: 5,
+		Left: &TreeNode{
+			Val: 1,
+		},
+	}
+
+	tree4 := &TreeNode{
+		Val: 5,
+		Right: &TreeNode{
+			Val: 7,
+			Right: &TreeNode{
+				Val: 9,
+			},
+		},
+	}
+	ans4 := &TreeNode{
+		Val: 5,
+		Right: &TreeNode{
+			Val: 9,
+		},
+	}
+
+	tree5 := &TreeNode{
+		Val: 5,
+		Right: &TreeNode{
+			Val: 7,
+			Left: &TreeNode{
+				Val: 6,
+			},
+			Right: &TreeNode{
+				Val: 14,
+				Left: &TreeNode{
+					Val: 12,
+					Left: &TreeNode{
+						Val: 8,
+						Right: &TreeNode{
+							Val: 9,
+						},
+					},
+					Right: &TreeNode{
+						Val: 13,
+					},
+				},
+			},
+		},
+	}
+	ans5 := &TreeNode{
+		Val: 5,
+		Right: &TreeNode{
+			Val: 8,
+			Left: &TreeNode{
+				Val: 6,
+			},
+			Right: &TreeNode{
+				Val: 14,
+				Left: &TreeNode{
+					Val: 12,
+					Left: &TreeNode{
+						Val: 9,
+					},
+					Right: &TreeNode{
+						Val: 13,
+					},
+				},
+			},
+		},
+	}
+
 	tests := []struct {
 		name   string
 		root   *TreeNode
@@ -66,7 +144,10 @@ func TestDeleteNode(t *testing.T) {
 	}{
 		{"example1", tree1, 3, ans1},
 		{"exampel2", tree2, 0, tree2},
-		{"example3", &TreeNode{}, 1, &TreeNode{}},
+		{"exampel3", tree3, 3, ans3},
+		{"exampel4", tree4, 7, ans4},
+		{"exampel5", tree5, 7, ans5},
+		{"example6", &TreeNode{}, 1, &TreeNode{}},
 	}
 
 	for _, tt := range tests {
