c7f65d99355f0b93bff0f71f3dcf302c00543ae5
xxxVitoxxx <bird06291994@gmail.com>
2024-02-28 18:18:35 +0800

    chore: leetcode649 one queue solution

--- 649.dota2_senate/main.go
+++ 649.dota2_senate/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 // queue
 // time complexity: O(n)
 // space complexity: O(n)
@@ -25,6 +27,106 @@ func predictPartyVictory(senate string) string {
 	}
 
 	if len(rQueue) > 0 {
+		return "Radiant"
+	}
+	return "Dire"
+}
+
+// one queue
+// time complexity: O(n)
+// space complexity: O(n)
+func predictPartyVictory2(senate string) string {
+	queue := []uint8(senate)
+	rCount := strings.Count(senate, "R")
+	dCount := strings.Count(senate, "D")
+	for rCount > 0 && dCount > 0 {
+		winParty := queue[0]
+		queue = queue[1:]
+		for i := 0; i < len(queue); i++ {
+			if queue[i] != winParty {
+				if queue[i] == 'R' {
+					rCount--
+				} else {
+					dCount--
+				}
+
+				queue = append(queue[:i], queue[i+1:]...)
+				break
+			}
+		}
+
+		queue = append(queue, winParty)
+	}
+
+	if rCount > 0 {
+		return "Radiant"
+	}
+	return "Dire"
+}
+
+// one queue
+// time complexity: O(n)
+// space complexity: O(n)
+func predictPartyVictory3(senate string) string {
+	parties := map[uint8]string{
+		'R': "Radiant",
+		'D': "Dire",
+	}
+
+	queue := []uint8(senate)
+	for len(queue) > 1 {
+		first := queue[0]
+		queue = queue[1:]
+
+		var i int
+		for ; i < len(queue); i++ {
+			if queue[i] != first {
+				queue = append(queue[:i], queue[i+1:]...)
+				break
+			}
+		}
+		if i == len(queue) {
+			return parties[first]
+		}
+
+		queue = append(queue, first)
+	}
+
+	return parties[queue[0]]
+}
+
+// optimize one queue
+// time complexity: O(n)
+// space complexity: O(n)
+func predictPartyVictory4(senate string) string {
+	queue := []uint8(senate)
+	rCount := strings.Count(senate, "R")
+	dCount := strings.Count(senate, "D")
+	rBan, dBan := 0, 0
+	for rCount > 0 && dCount > 0 {
+		curr := queue[0]
+		queue = queue[1:]
+
+		if curr == 'R' {
+			if rBan > 0 {
+				rBan--
+				rCount--
+			} else {
+				dBan++
+				queue = append(queue, curr)
+			}
+		} else {
+			if dBan > 0 {
+				dBan--
+				dCount--
+			} else {
+				rBan++
+				queue = append(queue, curr)
+			}
+		}
+	}
+
+	if rCount > 0 {
 		return "Radiant"
 	}
 	return "Dire"
--- 649.dota2_senate/main_test.go
+++ 649.dota2_senate/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPredictPartyVictory(t *testing.T) {
 	tests := []struct {
@@ -13,11 +15,39 @@ func TestPredictPartyVictory(t *testing.T) {
 		{"example3", "D", "Dire"},
 		{"example4", "RDR", "Radiant"},
 		{"example5", "DDRRR", "Dire"},
+		{"example6", "DRRDRDRDRDDRDRDR", "Radiant"},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			res := predictPartyVictory(tt.senate)
+			if res != tt.output {
+				t.Errorf("got %v, want %v", res, tt.output)
+			}
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := predictPartyVictory2(tt.senate)
+			if res != tt.output {
+				t.Errorf("got %v, want %v", res, tt.output)
+			}
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := predictPartyVictory3(tt.senate)
+			if res != tt.output {
+				t.Errorf("got %v, want %v", res, tt.output)
+			}
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := predictPartyVictory4(tt.senate)
 			if res != tt.output {
 				t.Errorf("got %v, want %v", res, tt.output)
 			}
