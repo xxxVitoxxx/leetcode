bb6f3dd2523d8219525d80bb1ddf133f116492c4
Vito <vito.chaooo@gmail.com>
2024-03-18 02:14:32 +0800

    chore: leetcode1926 (#144) (#145)

--- 1926.nearest_exit_from_entrance_in_maze/main.go
+++ 1926.nearest_exit_from_entrance_in_maze/main.go
@@ -9,14 +9,14 @@ func nearestExit(maze [][]byte, entrance []int) int {
 	row, col := len(maze), len(maze[0])
 	directions := [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
 	queue := [][]int{{entrance[0], entrance[1]}}
+	// mark the entrance as visited since its not a exit.
+	maze[entrance[0]][entrance[1]] = '+'
 	step := 0
 	for len(queue) > 0 {
 		l := len(queue)
 		for i := 0; i < l; i++ {
 			curr := queue[0]
 			queue = queue[1:]
-			// mark the entrance as visited since its not a exit.
-			maze[curr[0]][curr[1]] = '+'
 			if (curr[0] == 0 || curr[0] == row-1 || curr[1] == 0 || curr[1] == col-1) &&
 				(curr[0] != entrance[0] || curr[1] != entrance[1]) {
 				return step
@@ -27,6 +27,7 @@ func nearestExit(maze [][]byte, entrance []int) int {
 				nextRow, nextCol := curr[0]+d[0], curr[1]+d[1]
 				if nextRow >= 0 && nextRow < row && nextCol >= 0 && nextCol < col && maze[nextRow][nextCol] == '.' {
 					queue = append(queue, []int{nextRow, nextCol})
+					maze[nextRow][nextCol] = '+'
 				}
 			}
 		}
