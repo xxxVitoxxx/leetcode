4079cb8f41717be0d452f312e5bf9b5945b1d489
xxxVitoxxx <bird06291994@gmail.com>
2024-05-17 23:39:06 +0800

    chore: leetcode1141

---
+++ 1141.user_activity_for_the_past_30_days_I/main.sql
@@ -0,0 +1,11 @@
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM
+    Activity
+WHERE
+    DATEDIFF('2019-07-27', activity_date) < 30 AND DATEDIFF('2019-07-27', activity_date) >= 0
+    -- activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+    -- DATEDIFF('2019-07-27', activity_date) BETWEEN 0 AND 29
+GROUP BY
+    activity_date
