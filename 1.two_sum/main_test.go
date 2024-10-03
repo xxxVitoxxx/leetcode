2e570441bd56b39805e2c3cc01a4f8aaacc1be07
vito <vito.chaooo@gmail.com>
2024-10-03 15:54:06 +0800

    chore: leetcode1 add two pointer method (#196)
    * chore: leetcode1 add two pointer method
    
    * add test example

--- 1.two_sum/main_test.go
+++ 1.two_sum/main_test.go
@@ -16,11 +16,20 @@ func TestTwoSum(t *testing.T) {
 		{"example1", []int{2, 7, 11, 15}, 9, []int{0, 1}},
 		{"example2", []int{3, 2, 4}, 6, []int{1, 2}},
 		{"example3", []int{3, 3}, 6, []int{0, 1}},
+		{"example4", []int{11, 2, 7}, 18, []int{0, 2}},
+		{"example5", []int{9, 2, 5, 1}, 22, []int{}},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			res := twoSum(tt.input, tt.target)
+			assert.ElementsMatch(t, tt.output, res)
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := twoSum2(tt.input, tt.target)
 			assert.ElementsMatch(t, tt.output, res)
 		})
 	}
