a9c57e64d67ccec6e9f27e0202dd36c435598e6a
vito <vito.chaooo@gmail.com>
2024-10-12 01:57:51 +0800

    add leetcode 268 (#199)
    * add leetcode 268
    
    * add solution
    
    * update go mod
    
    * update import
    
    * update go mod

---
+++ 268.missing_number/main.go
@@ -0,0 +1,78 @@
+package main
+
+import "golang.org/x/exp/slices"
+
+// use hash
+// time complexity: O(n)
+// space complexity: O(n)
+func missingNumber(nums []int) int {
+	m := make(map[int]bool, len(nums)+1)
+	for i := 0; i <= len(nums); i++ {
+		m[i] = false
+	}
+
+	for _, n := range nums {
+		if _, ok := m[n]; ok {
+			m[n] = true
+		}
+	}
+
+	for k, v := range m {
+		if !v {
+			return k
+		}
+	}
+
+	return -1
+}
+
+// use slice
+// time complexity: O(n)
+// space complexity: O(n)
+func missingNumber2(nums []int) int {
+	arr := make([]bool, len(nums)+1)
+	for _, n := range nums {
+		arr[n] = true
+	}
+
+	for i := 0; i < len(arr); i++ {
+		if !arr[i] {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// use sort
+// time complexity: O(nk) an O(nk) worst case of pdqsort with k distinct elements
+// space complexity: O(1)
+func missingNumber3(nums []int) int {
+	slices.Sort(nums)
+
+	if nums[0] != 0 {
+		return 0
+	}
+	if nums[len(nums)-1] != len(nums) {
+		return len(nums)
+	}
+
+	for i, n := range nums {
+		if i != n {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// bit manipulation
+// time complexity: O(n)
+// space complexity: O(1)
+func missingNumber4(nums []int) int {
+	missing := len(nums)
+	for i, n := range nums {
+		missing ^= i ^ n
+	}
+	return missing
+}
