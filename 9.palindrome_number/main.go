0775bdc20a512e5aa5b67599d2619c79aa84fbd9
xxxVitoxxx <bird06291994@gmail.com>
2026-09-10 00:25:38 +0800

    refactor: leetcode 9

--- 9.palindrome_number/main.go
+++ 9.palindrome_number/main.go
@@ -1,14 +1,31 @@
 package main
 
-// follow up: can not converting the integer to a string
+import "strconv"
+
 func isPalindrome(x int) bool {
+	xStr := strconv.Itoa(x)
+	for left, right := 0, len(xStr)-1; left < right; {
+		if xStr[left] != xStr[right] {
+			return false
+		}
+
+		left++
+		right--
+	}
+	return true
+}
+
+// follow up: can not converting the integer to a string
+// time complexity: O(log10(N)). we divided the x by 10 for every iteration.
+// space complexity: O(1)
+func isPalindrome2(x int) bool {
 	if x < 0 {
 		return false
 	}
 
-	res, y := 0, x
-	for ; y > 0; y /= 10 {
-		res = res*10 + (y % 10)
+	var result int
+	for curr := x; curr > 0; curr /= 10 {
+		result = result*10 + curr%10
 	}
-	return res == x
+	return result == x
 }
